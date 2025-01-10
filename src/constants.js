c0d5ea10a3f67edd680b32f738473ee2fdc02f7c
BHAWANA SHARMA <bhawanaa604@gmail.com>
2025-01-10 21:03:35 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+export const scaleFactor = 4;
+
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript and Python these days, but I also enjoy exploring other languages like C++ and Java.  
+    I love programming and experimenting with web design tools like Wix. Check out my <a href="https://github.com/lavi445" target="_blank">GitHub</a> to see some of my work!`,
+    
+    "cs-degree": `This is my CS degree. As a third-year CSE student, I value the theoretical knowledge it provides, which complements my hands-on project experience.  
+    It's a journey that has shaped my understanding of computer science and engineering concepts.`,
+  
+    "sofa-table": `That's my sofa. I like to relax here and watch tech tutorials or edit videos.  
+    I also create educational content about web development on Instagram. Follow me on <a href="https://instagram.com/bhawana_as" target="_blank">Instagram</a> if you're curious!`,
+  
+    tv: `That's my TV. Recently, I've been enjoying content from creators like The Net Ninja, freeCodeCamp, and Traversy Media.  
+    They inspire my learning journey, especially in web development and UI/UX design.`,
+  
+    bed: `This is where I sleep and brainstorm ideas. Some of my best ideas for projects come when I'm lying here,  
+    and I always keep a notepad or my phone nearby to jot them down before I forget.`,
+  
+    resume: `This is my desk and on it is my resume. <a href="https://www.linkedin.com/in/bhawana-sharma-a0900025a/" target="_blank">Check it out?</a>  
+    Feel free to contact me via LinkedIn or email if you have any exciting opportunities in web development or UI/UX design!`,
+  
+    projects: `Info about this portfolio: I enjoy creating websites using tools like Wix and exploring frameworks like React.  
+    My recent projects include e-commerce websites, recipe games, and more. You can see some of my work on Fiverr:  
+    <a href="https://www.fiverr.com/s/YR0pV0R" target="_blank">Fiverr Gig</a>.`,
+  
+    library: `These shelves are filled with my technical books, including ones on algorithms, web development, and UX design.  
+    Though I don't always finish every book I start, I love learning and diving into new concepts.`,
+  
+    exit: `If you want to exit Bhawana Sharma's portfolio, just close the tab. Thanks for exploring my journey!`,
+  };
+  
